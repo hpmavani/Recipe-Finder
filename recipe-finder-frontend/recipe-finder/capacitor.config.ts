63b81c25b0a8f981bae02d33f4ba73b052006d5d
Hiya <hpmaav@gmail.com>
2025-01-29 20:07:09 -0500

    capacitor setup

---
+++ recipe-finder-frontend/recipe-finder/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'recipe-finder',
+  webDir: 'www'
+};
+
+export default config;
